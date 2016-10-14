587865ae1e963856dbe7ff0481cfb6937cddf9c3
Kirill Glazunov <glazunov@lingualeo.com>
2016-10-14 16:42:46 +0300

    Add an opportunity to get the hash from a directory recursively.

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ function md5FileSync (filename) {
   return hash.digest('hex')
 }
 
+function md5DirSync (dirname, ignorePaths) {
+  if (!fs.existsSync(dirname)) {
+    throw new Error(dirname + ' does not exist.')
+  } else if (!fs.statSync(dirname).isDirectory()) {
+    throw new Error(dirname + ' is not a directory.')
+  }
+
+  if (ignorePaths && Object.prototype.toString.call(ignorePaths).slice(8, -1) !== 'Array') {
+    ignorePaths = [ignorePaths]
+  }
+  for (var i = 0; i < ignorePaths.length; i++) {
+    ignorePaths[i] = fs.realpathSync(ignorePaths[i])
+  }
+
+  var files = fs.readdirSync(dirname)
+  var hash = crypto.createHash('md5')
+
+  for (i = 0; i < files.length; i++) {
+    var fullPath = fs.realpathSync(dirname + '/' + files[i])
+
+    if (ignorePaths.indexOf(fullPath) !== -1) {
+      continue
+    }
+
+    var fileStats = fs.statSync(fullPath)
+    var fileHash
+    if (fileStats.isDirectory()) {
+      fileHash = md5DirSync(fullPath, ignorePaths)
+    } else if (fileStats.isFile()) {
+      fileHash = md5FileSync(fullPath)
+    } else {
+      continue
+    }
+
+    hash.update(fileHash)
+  }
+
+  return hash.digest('hex')
+}
+
 function md5File (filename, cb) {
   if (typeof cb !== 'function') throw new TypeError('Argument cb must be a function')
 
@@ -43,3 +83,4 @@ function md5File (filename, cb) {
 
 module.exports = md5File
 module.exports.sync = md5FileSync
+module.exports.dirSync = md5DirSync
