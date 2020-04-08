c21358a11e49294eff65734ff3b559ae503a5647
Richie Bendall <richiebendall@gmail.com>
2020-04-09 01:49:22 +1200

    Simplify typings
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,15 @@
-/**
- * Asynchronously get the MD5-sum of the file at `path`
- *
- * @returns a `Promise` that will be resolved with a string containing the MD5-sum
- */
-declare function md5File (path: string): Promise<string>
+declare const md5File: {
+	/**
+	Asynchronously get the MD5-sum of the file at `path`
+	@returns A `Promise` that will be resolved with a string containing the MD5-sum.
+	*/
+	(path: string): Promise<string>
 
-/**
- * Synchronously get the MD5-sum of the file at `path`
- *
- * @returns a string containing the MD5-sum
- */
-declare function md5FileSync (path: string): string
+	/**
+	Synchronously get the MD5-sum of the file at `path`
+	@returns A string containing the MD5-sum.
+	*/
+	sync(path: string): string
+}
 
-declare const module: (typeof md5File) & { sync: typeof md5FileSync }
-export = module
+export = md5File
